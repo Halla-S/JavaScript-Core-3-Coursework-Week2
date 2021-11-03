76278b349b42ebeb86492f0d39dd7842a488a75d
Halla-S <86550752+Halla-S@users.noreply.github.com>
2021-11-03 13:17:12 +0000

    dog photos challenge completed

---
+++ challenges/dog-photo-gallery/script.js
@@ -0,0 +1,28 @@
+const photosDiv = document.getElementById("photos");
+const list = document.getElementById("list");
+function NewPhoto() {
+  const listItem = document.createElement("li");
+  listItem.style.listStyleType = "none";
+  list.className = "listItem";
+  const img = document.createElement("img");
+  fetch(`https://dog.ceo/api/breeds/image/random`)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+      throw `${response.status} ${response.statusText}`;
+    })
+    .then(function (data) {
+      img.src = `${data.message}`;
+      console.log(img.src);
+    })
+    .catch(function (error) {
+      console.log("An error occurred:", error);
+    });
+  list.appendChild(listItem);
+  listItem.appendChild(img);
+  list.style.display = "block";
+}
+function deletePhotos() {
+  list.innerHTML = "";
+}
